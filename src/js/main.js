76f12ad71e597f77e5443a94d49dfcc0a372d613
Leticia Cibele <leticiacibele.cl@gmail.com>
2026-05-20 09:00:04 -0300

    primeiro commit

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+async function carregarComponente(id, arquivo) {
+    
+    try {
+        const resposta = await fetch(arquivo);
+
+        if( !resposta.ok) {
+            throw new Error("Arquivo não encontrado")
+        }
+        const html = await resposta.text();
+
+        document.getElementById(id).innerHTML = html;
+    } catch(error) {
+        console.error(error);
+    }
+ 
+}
+
+carregarComponente("header", "/src/components/header.html")
